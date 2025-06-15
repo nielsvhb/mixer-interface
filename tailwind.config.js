ef9f8c21345a8b56a9d6c4019319b256f939ca4a
Niels Vanhorenbeeck <nielsvhb@hotmail.com>
2025-06-15 15:49:26 +0200

    Added some config for css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 ﻿module.exports = {
+    safelist: [],
     content: [
         "./**/*.razor",
         "./wwwroot/index.html",
@@ -10,7 +11,16 @@
           center: true,
           padding: '1rem'  
         },
-        extend: {},
+        extend: {
+            boxShadow: {
+                neumorphic: '10px 10px 30px #d1d9e6, -10px -10px 30px #ffffff',
+                'neumorphic-inset': 'inset 10px 10px 20px #d1d9e6, inset -10px -10px 20px #ffffff',
+                'neumorphic-light': '4px 4px 10px #c8d0e7, -4px -4px 10px #ffffff',
+            },
+            borderRadius: {
+                '3xl': '2rem',
+            }
+        },
     },
     plugins: [],
 }
