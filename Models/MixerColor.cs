fd5212c5c32e07d9ca422985da07c0018ca204eb
Niels Vanhorenbeeck <nielsvhb@hotmail.com>
2025-10-23 12:42:37 +0200

    Nieuwe kleur

--- Models/MixerColor.cs
+++ Models/MixerColor.cs
@@ -8,10 +8,10 @@ public class MixerColor : IntMappedEnumeration<MixerColor>
         HexCode = hexCode;
     }
 
-    public static readonly MixerColor Red = new MixerColor(nameof(Red), "RD", 1, "#FF0000");
-    public static readonly MixerColor Green = new MixerColor(nameof(Green), "GN", 2, "#00FF00");
-    public static readonly MixerColor Yellow = new MixerColor(nameof(Yellow), "YE", 3, "#FFFF00");
-    public static readonly MixerColor Blue = new MixerColor(nameof(Blue), "BL", 4, "#0000FF");
-    public static readonly MixerColor Magenta = new MixerColor(nameof(Magenta), "MG", 5, "#FF00FF");
-    public static readonly MixerColor Cyan = new MixerColor(nameof(Cyan), "CY", 6, "#00FFFF");
+    public static readonly MixerColor Red = new (nameof(Red), "RD", 1, "#FF0000");
+    public static readonly MixerColor Green = new (nameof(Green), "GN", 2, "#00FF00");
+    public static readonly MixerColor Yellow = new (nameof(Yellow), "YE", 3, "#FFFF00");
+    public static readonly MixerColor Blue = new (nameof(Blue), "BL", 4, "#0000FF");
+    public static readonly MixerColor Magenta = new (nameof(Magenta), "MG", 5, "#FF00FF");
+    public static readonly MixerColor White = new (nameof(White), "WH", 6, "#FFFFFF");
 }
